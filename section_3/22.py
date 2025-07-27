4a7a9a74e366ea52ea1b611facb81986592b49e8
anshdee99 <anshsingh171996@gmail.com>
2025-07-27 10:52:25 +0000

    Adding section3

---
+++ section_3/22.py
@@ -0,0 +1,6 @@
+print ("Welcome to the Roller Coaster")
+height = int(input("What is your height in cm ? "))
+if height >= 120: 
+    print ("You can ride the Roller Coaster")
+else: 
+    print ("You can not ride the Roller Coaster")
