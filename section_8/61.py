fc02743219a9593d51fee96c767542748f384737
EC2 Default User <anshsingh171996@gmail.com>
2025-08-09 19:32:26 +0000

    Functions with Inputs

---
+++ section_8/61.py
@@ -0,0 +1,15 @@
+def greet ():
+    print ("Hello there")
+    print ("How are you ?")
+    print ("What would you like to do ?")
+
+greet () #This is where the function is being called/triggered. 
+
+
+#Function that allows for input. 
+def greet_again(name):
+    print (f"Hello {name}")
+    print (f"How are you {name}?")
+    print (f"What would you like to do {name}?")
+
+greet_again("Ansh") 
